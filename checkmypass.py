d50da8d4366678cc81ab75e15d25cb51290c0677
saura <ehsaurav@gmail.com>
2021-01-09 18:02:53 +0530

    -

--- checkmypass.py
+++ checkmypass.py
@@ -7,7 +7,7 @@ def request_api_data(query_char):
     url = 'https://api.pwnedpasswords.com/range/' + query_char
     res = requests.get(url)
     if res.status_code != 200:
-        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again.')
+        raise RuntimeError(f'Error fetching: {res.status_code}, check the API and try again!')
     return res
 
 
@@ -21,8 +21,8 @@ def get_password_leaks_count(hashes, hash_to_check):
 
 def pwned_api_check(password):
     sha1password = hashlib.sha1(password.encode('utf-8')).hexdigest().upper()
-    first5char, tail = sha1password[:5], sha1password[5:]
-    response = request_api_data(first5char)
+    first5_char, tail = sha1password[:5], sha1password[5:]
+    response = request_api_data(first5_char)
     return get_password_leaks_count(response, tail)
 
 
@@ -30,11 +30,11 @@ def main(args):
     for password in args:
         count = pwned_api_check(password)
         if count:
-            print(f'{password} was found {count} times.')
+            print(f'{password} was found {count} time.. you should probably change your password.')
         else:
-            print(f'{password} was NOT found.')
+            print(f'{password} was not found. Carry on!')
     return 'done!'
 
 
 if __name__ == '__main__':
-    main(sys.argv[1:])
+    sys.exit(main(sys.argv[1:]))
